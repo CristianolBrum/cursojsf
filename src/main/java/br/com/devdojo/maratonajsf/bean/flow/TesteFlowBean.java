75455adaeb38b83fdf4880242e66ee171407a938
Cristiano Brum <cristianolbrum@gmail.com>
2019-10-17 23:22:41 -0300

    Adicionado flow scoped

---
+++ src/main/java/br/com/devdojo/maratonajsf/bean/flow/TesteFlowBean.java
@@ -0,0 +1,43 @@
+package br.com.devdojo.maratonajsf.bean.flow;
+
+import javax.faces.flow.FlowScoped;
+import javax.inject.Named;
+import java.io.Serializable;
+
+@Named
+@FlowScoped(value = "registration")
+public class TesteFlowBean implements Serializable {
+    private String nome,sobrenome,endereco;
+
+    public String salvar(){
+        System.out.println("Salvando no banco");
+        System.out.println(nome);
+        System.out.println(sobrenome);
+        System.out.println(endereco);
+        return "exitToInicio";
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+}
