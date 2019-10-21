4259e2ed572f3add4fe661d76a74c988245f782a
Cristiano Brum <cristiano.brum@datainfo.inf.br>
2019-10-21 12:47:21 -0400

    f param

--- src/main/java/br/com/devdojo/maratonajsf/bean/comunicacao/ComunicacaoResultadoBean.java
+++ src/main/java/br/com/devdojo/maratonajsf/bean/comunicacao/ComunicacaoResultadoBean.java
@@ -1,21 +1,17 @@
 package br.com.devdojo.maratonajsf.bean.comunicacao;
 
-import org.omnifaces.cdi.Param;
-
-import javax.annotation.PostConstruct;
 import javax.faces.view.ViewScoped;
-import javax.inject.Inject;
 import javax.inject.Named;
 import java.io.Serializable;
 
 @Named
 @ViewScoped
-public class ComunicacaoTeste3Bean implements Serializable {
+public class ComunicacaoResultadoBean implements Serializable {
     private String nome;
     private String sobreNome;
 
     public void init(){
-        System.out.println("Criou comunicacao 3");
+        System.out.println("Criou Comunicacao Resultado");
         System.out.println(nome);
         System.out.println(sobreNome);
     }
